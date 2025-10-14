62b74dd428211dd5c459a050c969f6386f2d9722
tylermong <117101211+tylermong@users.noreply.github.com>
2025-10-14 19:45:43 -0400

    chore: format code

--- magellan-tsp/src/main/java/edu/stockton/csci4510/team1/Main.java
+++ magellan-tsp/src/main/java/edu/stockton/csci4510/team1/Main.java
@@ -1,9 +1,5 @@
 package edu.stockton.csci4510.team1;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
-        
-    }
+public class Main {
+  public static void main(String[] args) {}
 }
