2b6fea0b8c4c02ac11f73ed6635ff694dc88f0b8
owen hanson <ambrynhanson@gmail.com>
2025-11-11 12:28:19 -0500

    fix pom & create hillclimb

---
+++ magellan-tsp/src/main/java/edu/stockton/csci4510/team1/magellantsp/HillClimbing.java
@@ -0,0 +1,23 @@
+package edu.stockton.csci4510.team1.magellantsp;
+
+import org.cicirello.search;
+
+public class HillClimbing{
+    public void steepest(){
+
+    }
+
+    public void first(){
+
+    }
+
+    public void random(){
+
+    }
+    public static void main(String[] args){
+        steepest();
+        first();
+        random();
+    }
+
+}
