999a2038b9378bbd630a821d531d5368e67b0e8c
owen hanson <ambrynhanson@gmail.com>
2025-12-12 20:44:08 -0500

    pre-pull request

--- magellan-tsp/src/main/java/edu/stockton/csci4510/team1/magellantsp/Main.java
+++ magellan-tsp/src/main/java/edu/stockton/csci4510/team1/magellantsp/Main.java
@@ -1,12 +1,36 @@
 package edu.stockton.csci4510.team1.magellantsp;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-  public static void main(String[] args) {
-    ArrayList<Airport> airports = AirportLoader.loadAirports("international_airports.csv");
+  List<Airport> airports = new ArrayList<>();
 
+  public static void main(String[] args) throws IOException {
+    ArrayList<Airport> airports = AirportLoader.loadAirports("international_airports.csv");
+    // double[][] dMatrix = HaversineDistance.buildDistanceMatrix(airports);
+    localSearchTSP(airports);
+    // crossoverTSP(airports, dMatrix);
+  }
+
+  public static void localSearchTSP(ArrayList<Airport> airports) {
     HillClimbingTSP hc = new HillClimbingTSP(airports);
     hc.runExperiment();
+  }
+
+  public void simmulatedAnnealingTSP() {
+    // add code here
+  }
+
+  public void mutationTSP() {
+    // add code here
+  }
+
+  public static void crossoverTSP(List<Airport> airports, double[][] distanceMatrix) {
+    // add code here
+  }
+
+  public void tylerTSP() {
+    // add code here
   }
 }
