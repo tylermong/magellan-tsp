8c4374886f637437cd218a25748d79c7ee1fe8f0
tylermong <117101211+tylermong@users.noreply.github.com>
2025-12-15 19:06:36 -0500

    feat: implement progress tracking

--- magellan-tsp/src/main/java/edu/stockton/csci4510/team1/magellantsp/HillClimbingTSP.java
+++ magellan-tsp/src/main/java/edu/stockton/csci4510/team1/magellantsp/HillClimbingTSP.java
@@ -32,14 +32,14 @@ public class HillClimbingTSP {
       ys[i] = airports.get(i).getLatitude();
     }
 
-    TSPEdgeDistance haversine =
-        new TSPEdgeDistance() {
-          @Override
-          public double distance(double x1, double y1, double x2, double y2) {
-            return HaversineDistance.calculateDistance(y1, x1, y2, x2);
-          }
-        };
-    // pulls the haversine distance to be used as the distance function for edge costs
+    TSPEdgeDistance haversine = new TSPEdgeDistance() {
+      @Override
+      public double distance(double x1, double y1, double x2, double y2) {
+        return HaversineDistance.calculateDistance(y1, x1, y2, x2);
+      }
+    };
+    // pulls the haversine distance to be used as the distance function for edge
+    // costs
     TSP.Double tsp = new TSP.Double(xs, ys, haversine);
 
     Configurator.configureRandomGenerator(213);
@@ -47,24 +47,47 @@ public class HillClimbingTSP {
     int restarts = 1; // anything but 1 was taking too long locally, must change !!!!!
 
     // fd = first descent, st steepest descent
-    SolutionCostPair<Permutation> fdRev =
-        new FirstDescentHillClimber<>(tsp, new ReversalMutation(), init).optimize(restarts);
-    SolutionCostPair<Permutation> fdIns =
-        new FirstDescentHillClimber<>(tsp, new InsertionMutation(), init).optimize(restarts);
-    SolutionCostPair<Permutation> fdSwap =
-        new FirstDescentHillClimber<>(tsp, new SwapMutation(), init).optimize(restarts);
-    SolutionCostPair<Permutation> stRev =
-        new SteepestDescentHillClimber<>(tsp, new ReversalMutation(), init).optimize(restarts);
-    SolutionCostPair<Permutation> stIns =
-        new SteepestDescentHillClimber<>(tsp, new InsertionMutation(), init).optimize(restarts);
-    SolutionCostPair<Permutation> stSwap =
-        new SteepestDescentHillClimber<>(tsp, new SwapMutation(), init).optimize(restarts);
+    int totalSteps = 6;
+    int currentStep = 1;
 
+    System.out.println(
+        "[" + currentStep + "/" + totalSteps + "] Running First Descent / Reversal...");
+    SolutionCostPair<Permutation> fdRev = new FirstDescentHillClimber<>(tsp, new ReversalMutation(), init)
+        .optimize(restarts);
     printResult("First Descent / Reversal", fdRev);
+    currentStep++;
+
+    System.out.println(
+        "[" + currentStep + "/" + totalSteps + "] Running First Descent / Insertion...");
+    SolutionCostPair<Permutation> fdIns = new FirstDescentHillClimber<>(tsp, new InsertionMutation(), init)
+        .optimize(restarts);
     printResult("First Descent / Insertion", fdIns);
+    currentStep++;
+
+    System.out.println("[" + currentStep + "/" + totalSteps + "] Running First Descent / Swap...");
+    SolutionCostPair<Permutation> fdSwap = new FirstDescentHillClimber<>(tsp, new SwapMutation(), init)
+        .optimize(restarts);
     printResult("First Descent / Swap", fdSwap);
+    currentStep++;
+
+    System.out.println(
+        "[" + currentStep + "/" + totalSteps + "] Running Steepest Descent / Reversal...");
+    SolutionCostPair<Permutation> stRev = new SteepestDescentHillClimber<>(tsp, new ReversalMutation(), init)
+        .optimize(restarts);
     printResult("Steepest Descent / Reversal", stRev);
+    currentStep++;
+
+    System.out.println(
+        "[" + currentStep + "/" + totalSteps + "] Running Steepest Descent / Insertion...");
+    SolutionCostPair<Permutation> stIns = new SteepestDescentHillClimber<>(tsp, new InsertionMutation(), init)
+        .optimize(restarts);
     printResult("Steepest Descent / Insertion", stIns);
+    currentStep++;
+
+    System.out.println(
+        "[" + currentStep + "/" + totalSteps + "] Running Steepest Descent / Swap...");
+    SolutionCostPair<Permutation> stSwap = new SteepestDescentHillClimber<>(tsp, new SwapMutation(), init)
+        .optimize(restarts);
     printResult("Steepest Descent / Swap", stSwap);
   }
 
